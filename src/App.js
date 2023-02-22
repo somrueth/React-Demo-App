43203654d69281be42b841ce89b0ae63f7c4e7f2
Somruethai Saiwan <36397387+minzsrt@users.noreply.github.com>
2023-02-22 11:42:46 +0700

    :sparkles: Create a demo project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1 class="text-blue-300 my-5 text-4xl font-bold">🔬 Lab : React x Tailwind</h1>
+
+        <div class="relative flex flex-col justify-center overflow-hidden">
+        <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+          <div class="mx-auto max-w-md">
+            <div class="divide-y divide-gray-300/50">
+              <div class="space-y-6 py-8 text-base leading-7 text-gray-600">
+                <p>An advanced online playground for Tailwind CSS, including support for things like:</p>
+                <ul class="space-y-4">
+                  <li class="flex items-center">
+                    <svg class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+                      <circle cx="12" cy="12" r="11" />
+                      <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+                    </svg>
+                    <p class="ml-4">
+                      Customizing your
+                      <code class="text-sm font-bold text-gray-900">tailwind.config.js</code> file
+                    </p>
+                  </li>
+                  <li class="flex items-center">
+                    <svg class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+                      <circle cx="12" cy="12" r="11" />
+                      <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+                    </svg>
+                    <p class="ml-4">
+                      Extracting classes with
+                      <code class="text-sm font-bold text-gray-900">@apply</code>
+                    </p>
+                  </li>
+                  <li class="flex items-center">
+                    <svg class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" stroke-linecap="round" stroke-linejoin="round">
+                      <circle cx="12" cy="12" r="11" />
+                      <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+                    </svg>
+                    <p class="ml-4">Code completion with instant preview</p>
+                  </li>
+                </ul>
+                <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
+              </div>
+              <div class="pt-8 text-base font-semibold leading-7">
+                <p class="text-gray-900">Want to dig deeper into Tailwind?</p>
+                <p>
+                  <a href="https://tailwindcss.com/docs" class="text-sky-500 hover:text-sky-600">Read the docs &rarr;</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+
       </header>
+
     </div>
   );
 }
